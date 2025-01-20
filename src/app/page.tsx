7f0b8a2d259ef142ed560ac2ad4cd25fc75f8d9f
pasha <mtipasha2021@gmail.com>
2025-01-20 09:56:06 +0330

    little change at style of Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,32 @@
-import Hero from "@/components/Hero";
-import Nav from "@/components/Nav";
-import { Container } from "@mui/material";
+import type { NextPage } from "next";
+import Nav from "../components/Nav";
+import { Box, Container } from "@mui/material";
+import Hero from "../components/Hero";
 
-export default function Home() {
+const Home: NextPage = () => {
   return (
     <div>
-      <Container>
-        <Nav />
-        <hr />
-        <Hero />
-      </Container>
+      <div
+        style={{
+          backgroundImage: "url(./assets/home/desktop/image-hero.jpg)",
+          height: "100vh",
+          backgroundSize: "100%",
+          color: "white",
+        }}
+      >
+        <Container>
+          <Nav />
+          <hr></hr>
+          <Hero />
+        </Container>
+      </div>
+      <Box sx={{ height: "30vh" }}>
+        <Container
+          sx={{ display: "flex", justifyContent: "space-between" }}
+        ></Container>
+      </Box>
     </div>
   );
-}
+};
+
+export default Home;
